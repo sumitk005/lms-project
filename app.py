def81c4f6fbee71790ea897775f0250a7a342c20
Sumit Kokare <sumitkokare9010@gmail.com>
2026-03-21 21:44:33 +0530

    fix port issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
+import os
 from datetime import datetime
 
 app = Flask(__name__)
@@ -401,4 +402,4 @@ def add_user():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",10000)))
